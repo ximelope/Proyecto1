f0e55afefe5105b6532a1cf2961c6dc88660ff74
JuliannaFFFF <lj.ferreira@uniandes.edu.co>
2024-04-19 11:02:29 -0500

    a

---
+++ src/modeloGaleria/Subasta.java
@@ -0,0 +1,11 @@
+package modeloGaleria;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Subasta {
+	private String fechaInicial;
+	private String fechaFinal;
+	private 
+}
