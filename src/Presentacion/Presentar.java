7fa758bebe3d94f6e195528a6e1f0ead8c00c950
angie <angie@XimenaLopez>
2024-04-20 13:17:35 -0500

    cambio

---
+++ src/Presentacion/Presentar.java
@@ -0,0 +1,13 @@
+package Presentacion;
+import modeloGaleria.Galeria;
+import modeloGaleria.Administrador;
+
+public class Presentar {
+	Galeria galeria = new Galeria();
+	public void mostrarMenu() {
+		System.out.println("1.) Login");
+		System.out.println("2.) Salir ");
+	}
+		
+
+}
---
+++ src/modeloGaleria/Impresion.java
@@ -0,0 +1,32 @@
+package modeloGaleria;
+
+public class Impresion extends Pieza {
+	private float resolucion;
+	private String tecnica;
+	private float ancho;
+	private float alto;
+	public Impresion(String tipo, String titulo, int ano, String lugarCreacion, String autor, boolean exhibida,
+			boolean permisoVenta, float valorFijo, double valorMinimoSubasta, Propietario propietario,
+			String estadoDePieza, float resolucion, String tecnica, float ancho, float alto) {
+		super(tipo, titulo, ano, lugarCreacion, autor, exhibida, permisoVenta, valorFijo, valorMinimoSubasta, propietario,
+				estadoDePieza);
+		this.resolucion= resolucion;
+		this.tecnica= tecnica; 
+		this.ancho= ancho;
+		this.alto = alto;
+		// TODO Auto-generated constructor stub
+	}
+	public float getResolucion() {
+		return resolucion;
+	}
+	public String getTecnica() {
+		return tecnica;
+	}
+	public float getAncho() {
+		return ancho;
+	}
+	public float getAlto() {
+		return alto;
+	}
+
+}
