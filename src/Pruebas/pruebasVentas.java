60b0c54ef3d0b2af239b6389c9b27e28c56511b0
JuliannaFFFF <lj.ferreira@uniandes.edu.co>
2024-05-07 06:43:19 -0500

    xd

--- src/Pruebas/pruebasVentas.java
+++ src/Pruebas/pruebasVentas.java
@@ -1,5 +1,5 @@
 package Pruebas;
-import static org.junit.Assert.assertEquals;x
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
